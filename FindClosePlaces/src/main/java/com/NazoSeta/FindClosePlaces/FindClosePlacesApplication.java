c6d8a83292fe3e64bcd808f9662619fa83a69e6d
N. Ozan Ateş <niyaziozanates@gmail.com>
2025-02-16 16:58:06 +0300

    Initial Backend push

---
+++ FindClosePlaces/src/main/java/com/NazoSeta/FindClosePlaces/FindClosePlacesApplication.java
@@ -0,0 +1,13 @@
+package com.NazoSeta.FindClosePlaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindClosePlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindClosePlacesApplication.class, args);
+	}
+
+}
---
+++ FindClosePlaces/src/test/java/com/NazoSeta/FindClosePlaces/FindClosePlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NazoSeta.FindClosePlaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindClosePlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
