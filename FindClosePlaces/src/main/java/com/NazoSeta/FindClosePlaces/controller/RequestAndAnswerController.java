ee69c7486b186ecc54ba7e1c4e6d36ad7d884bb1
N. Ozan Ateş <niyaziozanates@gmail.com>
2025-02-16 18:56:33 +0300

    Created the controller and service class for CRUD operations. These are just made for now and will later be updated based on the needings that we will see while creating the Front-End.

--- FindClosePlaces/src/main/java/com/NazoSeta/FindClosePlaces/controller/RequestAndAnswerController.java
+++ FindClosePlaces/src/main/java/com/NazoSeta/FindClosePlaces/controller/RequestAndAnswerController.java
@@ -1,4 +1,44 @@
 package com.NazoSeta.FindClosePlaces.controller;
 
+import com.NazoSeta.FindClosePlaces.dto.RequestAndAnswerDTO;
+import com.NazoSeta.FindClosePlaces.model.RequestAndAnswer;
+import com.NazoSeta.FindClosePlaces.service.RequestAndAnswerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class RequestAndAnswerController {
+
+    @Autowired
+    private final RequestAndAnswerService requestAndAnswerService;
+
+    @PostMapping("/addlocation")
+    public RequestAndAnswerDTO addLocation(@RequestBody RequestAndAnswer requestAndAnswer) {
+        return requestAndAnswerService.addLocation(requestAndAnswer);
+    }
+
+    @GetMapping("/getalllocations")
+    public List<RequestAndAnswerDTO> getAllLocations(){
+        return requestAndAnswerService.getAllLocations();
+    }
+
+    @GetMapping("/getlocation/{id}")
+    public RequestAndAnswerDTO getLocation(@PathVariable Long id) {
+        return requestAndAnswerService.getLocation(id);
+    }
+
+    @PutMapping("/updatelocation/{id}")
+    public RequestAndAnswerDTO updateLocation(@RequestBody RequestAndAnswer requestAndAnswer, @PathVariable Long id) {
+        return requestAndAnswerService.updateLocation(requestAndAnswer, id);
+    }
+
+    @DeleteMapping("/deletelocation/{id}")
+    public RequestAndAnswerDTO deleteLocation(@PathVariable Long id){
+        return requestAndAnswerService.deleteLocation(id);
+    }
+
 }
--- FindClosePlaces/src/main/java/com/NazoSeta/FindClosePlaces/service/RequestAndAnswerService.java
+++ FindClosePlaces/src/main/java/com/NazoSeta/FindClosePlaces/service/RequestAndAnswerService.java
@@ -1,4 +1,79 @@
 package com.NazoSeta.FindClosePlaces.service;
 
+import com.NazoSeta.FindClosePlaces.dto.RequestAndAnswerDTO;
+import com.NazoSeta.FindClosePlaces.model.RequestAndAnswer;
+import com.NazoSeta.FindClosePlaces.repository.RequestAndAnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class RequestAndAnswerService {
+
+    private final RequestAndAnswerRepository requestAndAnswerRepository;
+
+    public RequestAndAnswerDTO addLocation(RequestAndAnswer requestAndAnswer) {
+
+        requestAndAnswerRepository.save(requestAndAnswer);
+        ModelMapper modelMapper = new ModelMapper();
+        RequestAndAnswerDTO requestAndAnswerDTO = modelMapper.map(requestAndAnswer, RequestAndAnswerDTO.class);
+
+        return requestAndAnswerDTO;
+    }
+
+    public List<RequestAndAnswerDTO> getAllLocations() {
+
+        List<RequestAndAnswer> requestAndAnswerList = requestAndAnswerRepository.findAll();
+        ModelMapper modelMapper = new ModelMapper();
+        List<RequestAndAnswerDTO> requestAndAnswerDTOList = Arrays.asList(modelMapper.map(requestAndAnswerList, RequestAndAnswerDTO[].class));
+
+        return requestAndAnswerDTOList;
+    }
+
+    public RequestAndAnswerDTO getLocation(Long id) {
+
+        RequestAndAnswer requestAndAnswer = requestAndAnswerRepository.findById(id).orElseThrow();
+        ModelMapper modelMapper = new ModelMapper();
+        RequestAndAnswerDTO requestAndAnswerDTO = modelMapper.map(requestAndAnswer, RequestAndAnswerDTO.class);
+
+        return requestAndAnswerDTO;
+    }
+
+    public RequestAndAnswerDTO updateLocation(RequestAndAnswer newRequestAndAnswer, Long id){
+
+        RequestAndAnswer requestAndAnswer = requestAndAnswerRepository.findById(id).orElseThrow();
+
+        if (newRequestAndAnswer.getLongitude() != null) {
+            requestAndAnswer.setLongitude(newRequestAndAnswer.getLongitude());
+        }
+        if (newRequestAndAnswer.getLatitude() != null) {
+            requestAndAnswer.setLatitude(newRequestAndAnswer.getLatitude());
+        }
+        if (newRequestAndAnswer.getRadius() != null) {
+            requestAndAnswer.setRadius(newRequestAndAnswer.getRadius());
+        }
+
+        requestAndAnswerRepository.save(requestAndAnswer);
+
+        ModelMapper modelMapper = new ModelMapper();
+        RequestAndAnswerDTO requestAndAnswerDTO = modelMapper.map(requestAndAnswer, RequestAndAnswerDTO.class);
+
+        return requestAndAnswerDTO;
+    }
+
+    public RequestAndAnswerDTO deleteLocation(Long id){
+
+        RequestAndAnswer requestAndAnswer = requestAndAnswerRepository.findById(id).orElseThrow();
+        ModelMapper modelMapper = new ModelMapper();
+        RequestAndAnswerDTO requestAndAnswerDTO = modelMapper.map(requestAndAnswer, RequestAndAnswerDTO.class);
+
+        requestAndAnswerRepository.delete(requestAndAnswer);
+
+        return requestAndAnswerDTO;
+    }
+
 }
